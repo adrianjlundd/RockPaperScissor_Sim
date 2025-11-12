eb59c3877ed3bac45be3813690407302f38db687
Adrian Jacobsen Lund <adriajl@stud.ntnu.no>
2025-11-12 17:45:03 +0100

    Add configs to main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from world import World
-from agent import Agent
-from simulation import Simulation
-
 # ========================================
 # CONFIGURATION
 # ========================================
 
-BOX_SIZE = 1.0
-SPEED = 0.01
-INTERACTION_RADIUS = 0.02
-N_AGENTS = 300
-FRACTIONS = (0.33, 0.33, 0.34)  # (Rock, Paper, Scissors)
-MAX_STEPS = 5000
-WRAP = True
-SEED = 42
+import numpy as np
+from simulation import Simulation
+
+#Global simulation parameters
+
+CONFIG  = {
+
+    "BOX_SIZE": 1.0,
+    "SPEED": 0.01,
+    "INTERACTION_RADIUS": 0.02,
+    "N_AGENTS": 300,
+    "FRACTIONS": (0.33, 0.33, 0.34),  # (Rock, Paper, Scissors)
+    "MAX_STEPS": 5000,
+    "WRAP": True,
+    "SEED": 42
+
+}
 
 np.random.seed(SEED)
 
